a6e2f38aab09ed6df2b5955dfa64546e1a9371a1
Mahmut Uludag <mahmut.uludag@kaust.edu.sa>
2022-07-26 19:12:07 +0300

    xml parser; initial support for sequences indexed w/o the -parse_seqids option

--- bin/searchresults.py
+++ bin/searchresults.py
@@ -29,20 +29,29 @@ def tabulardatafromxml(inf):
 
 def process_iteration(itr, qlen):
     query_id = itr['Iteration_query-def'].split(' ')[0]
-    print("# %d similar sequences found" % len(itr['Iteration_hits']['Hit']))
-    for sseqs in itr['Iteration_hits']['Hit']:
-        subject_id = sseqs['Hit_id']
-        if isinstance(sseqs['Hit_hsps']['Hsp'], list):
-            coverage = sum([(int(hsp['Hsp_query-to']) -
-                             int(hsp['Hsp_query-from']))
-                            for hsp in sseqs['Hit_hsps']['Hsp']])
-            for hsp in sseqs['Hit_hsps']['Hsp']:
-                print_hsp(hsp, query_id, subject_id, coverage, qlen)
-        else:
-            hsp = sseqs['Hit_hsps']['Hsp']
-            coverage = (int(hsp['Hsp_query-to']) -
-                        int(hsp['Hsp_query-from']))
+    if isinstance(itr['Iteration_hits']['Hit'], list):
+        print("# %d similar sequences found" % len(itr['Iteration_hits']['Hit']))
+        for sseqs in itr['Iteration_hits']['Hit']:
+            process_similar_sequence(sseqs, query_id, qlen)
+    else:
+        print("# 1 similar sequence found")
+        process_similar_sequence(itr['Iteration_hits']['Hit'], query_id, qlen)
+
+def process_similar_sequence(sseqs, query_id, qlen):
+    subject_id = sseqs['Hit_id']
+    if "BL_ORD_ID" in subject_id:
+        subject_id = sseqs['Hit_def'].split(' ')[0]
+    if isinstance(sseqs['Hit_hsps']['Hsp'], list):
+        coverage = sum([(int(hsp['Hsp_query-to']) -
+                         int(hsp['Hsp_query-from']))
+                        for hsp in sseqs['Hit_hsps']['Hsp']])
+        for hsp in sseqs['Hit_hsps']['Hsp']:
             print_hsp(hsp, query_id, subject_id, coverage, qlen)
+    else:
+        hsp = sseqs['Hit_hsps']['Hsp']
+        coverage = (int(hsp['Hsp_query-to']) -
+                    int(hsp['Hsp_query-from']))
+        print_hsp(hsp, query_id, subject_id, coverage, qlen)
 
 
 def test_readxml():
